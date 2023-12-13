0782fc7fe6c05bf8c2ff623c44670beb3465b695
Gautam-2604 <gaurisariag@gmail.com>
2023-12-13 11:36:55 +0530

    Utils code discussion

--- src/index.js
+++ src/index.js
@@ -2,10 +2,19 @@ import mongoose from "mongoose"
 import {DB_NAME} from "./constants.js"
 import dotenv from "dotenv"
 import {connectDB} from "./db/index.js"
+import { app } from "./app.js"
 dotenv.config({
     path:'./env'
 })
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000 , ()=>{
+        console.log(` Server is running at port : ${process.env.PORT}`)
+    })
+})
+.catch((err)=>{
+    console.log("Connection failed");
+})
 /*
 import express from "express"
 const app=express()
